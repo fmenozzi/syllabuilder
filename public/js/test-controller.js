0f4919c645ed21a741f8d80c3910cc6b4e201a73
Federico Menozzi <federicogmenozzi@gmail.com>
2016-02-16 12:43:01 -0500

    MEAN skeleton up and running

--- public/js/test-controller.js
+++ public/js/test-controller.js
@@ -1,70 +1,68 @@
 var app = angular.module('test-app', []);
 
 app.controller('basic-name-controller', function($scope) {
-	// Default name in first input field
-	$scope.firstName = "John";
-	$scope.lastName = "Doe";
-	$scope.fullName = function() {
-		return $scope.firstName + " " + $scope.lastName;
-	};
+    // Default name in first input field
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    };
 
-	// Default color in second input field
-	$scope.inputcolor = "red";
+    // Default color in second input field
+    $scope.inputcolor = "red";
 });
 
 app.controller('ng-repeat-controller', function($scope) {
-	$scope.people = [
+    $scope.people = [
         {name: 'Fed',     gender: 'Male'},
-		{name: 'Cassidy', gender: 'Male'},
-		{name: 'Mark',    gender: 'Male'},
-		{name: 'Devi',    gender: 'Female'},
+        {name: 'Cassidy', gender: 'Male'},
+        {name: 'Mark',    gender: 'Male'},
+        {name: 'Devi',    gender: 'Female'},
     ];
 });
 
 app.controller('filter-controller', function($scope) {
-	$scope.names = [
-		"Abigail",
-		"Jeremy",
-		"Andrew",
-		"Anthony",
-		"Betsy",
-		"Bernard",
-		"John",
-		"Cassie",
-	];
+    $scope.names = [
+        "Abigail",
+        "Jeremy",
+        "Andrew",
+        "Anthony",
+        "Betsy",
+        "Bernard",
+        "John",
+        "Cassie",
+    ];
 });
 
 app.controller('basic-services-controller', function($scope, $location, $interval) {
-	// Show absolute path of index.html
-	$scope.url = $location.absUrl();
+    // Show absolute path of index.html
+    $scope.url = $location.absUrl();
 
-	// Display current time every second
-	$scope.time = new Date().toLocaleTimeString();
-	$interval(function() {
+    // Display current time every second
+    $scope.time = new Date().toLocaleTimeString();
+    $interval(function() {
         $scope.time = new Date().toLocaleTimeString();
     }, 1000);
 });
 
 app.controller('basic-http-controller', function($scope, $http) {
-	// Use server at localhost:8000
-	var baseURL = "http://localhost:8000";
-	var getPath = "/test";
+    // Use server at localhost:8000
+    var baseURL = "http://localhost:8000";
+    var getPath = "/test";
 
-	$scope.serverURL = baseURL + getPath;
+    $scope.serverURL = baseURL + getPath;
 
-	// Make GET request to retrieve sample JSON
-	$http.get(getPath).then(
-	function(resp) { // Success
-        $scope.resp = resp.data.mydata;
-    },
-    function(resp) { // Failure
-    	$scope.resp = "Failure! " + resp.status;
-    });
+    // Success and failure callbacks
+    var success = function(resp) {$scope.titles = resp.data.titles;};
+    var failure = function(resp) {$scope.titles = "Failure! " + resp.status;};
+
+    // Make GET request to retrieve sample JSON using query "?titles=all"
+    $http.get(getPath, {params: {titles: "all"}}).then(success, failure);
 });
 
 app.controller('dragula-controller', function($scope) {
-	$scope.item1 = "Item 1";
-	$scope.item2 = "Item 2";
-	$scope.item3 = "Item 3";
-	$scope.item4 = "Item 4";
+    $scope.item1 = "Item 1";
+    $scope.item2 = "Item 2";
+    $scope.item3 = "Item 3";
+    $scope.item4 = "Item 4";
 });
--- server.js
+++ server.js
@@ -1,4 +1,48 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+/*
+ * Mongoose
+ */
+
+// Connect to MongoDB database
+mongoose.connect("mongodb://localhost/test");
+
+// Open database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {console.log("We're connected!");});
+
+// Define Section schema
+var sectionSchema = mongoose.Schema({title: String});
+var Section = mongoose.model('Section', sectionSchema);
+
+// Delete database (so that we have a fresh copy every time we connect)
+Section.remove({}, function(err) {
+    console.log('collection removed');
+});
+
+// Make three sections
+var section1 = new Section({title: "Course Name"});
+var section2 = new Section({title: "Learning Objectives"});
+var section3 = new Section({title: "Prerequisites"});
+
+// DB insert callback
+var saveCallback = function(err, section) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("Inserting " + section.title);
+};
+
+// Save sections to database
+section1.save(saveCallback);
+section2.save(saveCallback);
+section3.save(saveCallback);
+
+/*
+ * Express
+ */
 
 var app = express();
 
@@ -16,6 +60,24 @@ var server = app.listen(8000, 'localhost', function() {
 
 // Handle GET requests to /test
 app.get('/test', function(req, res) {
-    res.status(200).json({mydata: "Hello from Express!"});
+    // Log query parameters from GET request
+    console.log("GET request params: " + req.query.titles);
+
+    // Return JSON object with all titles from the database
+    if (req.query.titles === "all") {
+        Section.find(function(err, sections) {
+            if (err) {
+                res.status(404).json({});
+            } else {
+                var allTitles = [];
+                for (var i = 0; i < sections.length; i++) {
+                    allTitles.push(sections[i].title);
+                }
+                res.status(200).json({titles: allTitles});
+            }
+        });
+    } else {
+        res.status(404).json({});
+    }
 });
 
